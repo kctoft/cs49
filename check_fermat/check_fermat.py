4c6beecf04fc81499a12dc2748589570c57ed634
Kitana Toft <kittoft@gmail.com>
2015-05-13 23:20:13 -0700

    "Fermat's theorum - a^n + b^n = c^n when n >2

---
+++ check_fermat/check_fermat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+#
+# Fermat's Last Theorem says that there are no positive integers a, b, and c
+#  such that a^n + b^n = c^n
+#
+# Write a function named check_fermat that takes four parameter: xa, b, c and n
+#and that for any values of n greater than 2.
+#
+# checks to see if Fermat's theorem holds.
+# If n is greater than 2 and it turns out to be true that an + bn = cn,
+# the program should print, 'Holy smokes, Fermat was wrong!'
+# Otherwise, the program should print, 'No, that doesn't work.'
+
+a = 3
+b = 4
+c = 5
+n = 3
+
+def check_fermat(a, b, c, n):
+    if a**n + b**n == c**n and n > 2:
+        print("Holy smokes, Fermat was wrong!")
+    else:
+        print("No, that does not work.")
+
+print(check_fermat(a, b, c, n))
