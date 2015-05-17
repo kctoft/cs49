824d325cb00ba368784ad1bccf238e219861a9e2
Kitana Toft <kittoft@gmail.com>
2015-05-16 20:36:24 -0700

    sum of 1+3+5+...(2n-1) while loop

--- lab5/sum_odd.py
+++ lab5/sum_odd.py
@@ -3,12 +3,11 @@
 #Q; Write a while loop fragment that calculates the following values...
 # Sum of the first n odd numbers: 1+3+5+ ... + (2n - 1)
 
+print("A program that finds out the sum of the first odd #'s: 1+3+5+...+n (2n -1)")
 n = int(input("Enter a number for 'n': "))
 def sum_odd(n):
-    print("A program that finds out the sum of the first n odd #'s: 1+3+5+ ... +n")
     sum = 0
     i = 1
-#    n = int(input("Please enter a number for 'n': "))
     while i <= n:
         sum = sum + ((2*i) - 1)
         i = i + 1
