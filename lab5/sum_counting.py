0c634e7c3931962a02071882c8456a2a620daa5b
Kitana Toft <kittoft@gmail.com>
2015-05-16 20:28:33 -0700

    while loop 1+2+3+ ... + n

--- lab5/sum_counting.py
+++ lab5/sum_counting.py
@@ -4,15 +4,15 @@
 # (a) Sum of the first n countring numbers: 1 + 2 + 3 + ... + n
 
 print("A program that finds the sum of 1+2+3 ... +n")
+n = int(input("Please enter a value for 'n': "))
 def sum_counting(n): #takes the parameter 'n'
     sum = 0 #declare the variable
     i = 1 #ensures the value for n remains unchanged
     while i <=  n:
         sum = sum + i
         i = i + 1
+    print("The answer is:")
     return sum #returns the new value for 'sum'
 
-
-#testing my logic
-print(sum_counting(4) == 10) #should print true
-print(sum_counting(1000) == 500500) #should print true
+#tests
+print(sum_counting(n))
